1d0b16b97fa4fd621a1c9140f2f4e281df72b626
bagasysf <bagasysf@gmail.com>
2021-03-04 19:29:14 +0700

    merevisi nama aplikasi menjadi reorder

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $title = 'Category';
+        $category = Category::all();
+        return view('category.index', [
+            'title' => $title,
+            'category' => $category
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Category::create([
+            'name' => request('name'),
+            'description' => request('description')
+        ]);
+
+        return redirect('/category');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Category $category, $id)
+    {
+        $title = 'Edit Category';
+        $category = Category::all();
+        $editcategory = Category::where('id', $id)->first();
+        return view('category.edit', [
+            'title' => $title,
+            'category' => $category,
+            'editcategory' => $editcategory
+        ]);
+
+        return redirect('category.edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Category $category, $id)
+    {
+        $category = Category::where('id', $id)->first();
+        $category->update([
+            'name' => request('name'),
+            'description' => request('description')
+        ]);
+
+        return redirect('/category');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category, $id)
+    {
+        $category = Category::where('id', $id)->first();
+        $category->delete();
+
+        return redirect('/category');
+    }
+}
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,5 @@
+@extends('template.base')
+
+@section('content')
+<button type="button" class="btn btn-transparent btn-lg shadow fw-bold mt-5 fs-6">Dashboard</button>
+@endsection
