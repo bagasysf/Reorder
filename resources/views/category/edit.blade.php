40ecc9450c8ff224e2267953f7b3f874ced13acd
bagasysf <bagasysf@gmail.com>
2021-03-06 16:14:28 +0700

    merubah design pure bootsrap 5

--- resources/views/category/edit.blade.php
+++ resources/views/category/edit.blade.php
@@ -3,86 +3,90 @@
 @section('title', $title)
 
 @section('content')
-<div class="row mt-5">
-    <div class="col-2">
-        <button class="btn btn-transparent btn-lg shadow fw-bold fs-6" style="width: auto; height: 48px;">Category</button>
-    </div>
-    <div class="col-4 ms-4">
-        <div class="input-group" style="width: 100%; height: 48px;">
-            <input type="text" class="form-control rounded-pill ps-4 text-secondary" style="font-size: 14px;" placeholder="Search something here .." aria-label="Username" aria-describedby="basic-addon1">
+<div class="row">
+    <div class="col-7">
+        <div class="row pt-5 ps-5">
+            <div class="col-5">
+                <button class="btn btn-transparent btn-lg shadow fw-bold fs-6" style="width: auto; height: 48px;">Category</button>
+            </div>
+            <div class="col-7">
+                <div class="input-group" style="height: 48px;">
+                    <input type="text" class="form-control rounded-pill ps-4" style="font-size: 14px;" placeholder="Search something here .." aria-label="Username" aria-describedby="basic-addon1">
+                    <button class="btn btn-transparent shadow rounded-circle ms-4" style="width: 48px; height: 48px;"><img src="{{ asset('styles/img/icon/search.svg') }}" alt=""></button>
+                </div>
+            </div>
         </div>
-    </div>
-    <div class="col-1 d-flex justify-content-end" style="width: auto;">
-        <button class="btn btn-transparent shadow rounded-circle d-flex justify-content-center align-items-center" style="width: 48px; height: 48px;"><img src="{{ asset('styles/img/icon/search.svg') }}" alt=""></button>
+        <div class="row ps-5">
+            <div class="col mt-4">
+                <div class="shadow px-3 py-1 rounded" style="width: 100%;">
+                    <table class="table table-borderless lh-lg">
+                        <thead>
+                            <tr>
+                                <th>Number</th>
+                                <th>Name</th>
+                                <th>Desription</th>
+                                <th>Customize</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @php
+                            $num=0;
+                            @endphp
+
+                            @foreach($category as $category)
+                            @php
+                            $num++;
+                            @endphp
+                            <tr>
+                                <td>{{$num}}</td>
+                                <td>{{$category->name}}</td>
+                                <td>{{$category->description}}</td>
+                                <td>
+                                    <form action="/category/{{$category->id}}" method="POST">
+                                        @csrf
+                                        @method("DELETE")
+                                        <a href="{{ url('category', $category->id) }}/edit " class="text-decoration-none">
+                                            <img src="{{ asset('styles/img/icon/edit-2.svg') }}" class="pe-3" alt="">
+                                        </a>
+                                        <button type="submit" class="btn btn-transparent p-0 mb-1">
+                                            <img src="{{ asset('styles/img/icon/trash-2.svg') }}" alt="">
+                                        </button>
+                                    </form>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
     </div>
     <div class="col-5">
-        <button class="btn btn-transparent btn-lg shadow ms-2 fw-bold fs-6 text-primary" style="width: auto; height: 48px;">Edit Category</button>
-    </div>
-</div>
-<div class="row">
-    <div class="col-7 mt-4">
-        <div class="shadow px-3 py-1 rounded" style="width: 100%;">
-            <table class="table table-borderless lh-lg">
-                <thead>
-                    <tr>
-                        <th>Number</th>
-                        <th>Name</th>
-                        <th>Desription</th>
-                        <th>Customize</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @php
-                    $num=0;
-                    @endphp
-
-                    @foreach($category as $category)
-                    @php
-                    $num++;
-                    @endphp
-                    <tr>
-                        <td>{{$num}}</td>
-                        <td>{{$category->name}}</td>
-                        <td>{{$category->description}}</td>
-                        <td>
-                            <form action="/category/{{$category->id}}" method="POST">
-                                @csrf
-                                @method("DELETE")
-                                <!-- <a href="category/{{$category->id}}/edit" class="text-decoration-none">
-                                    <img src="{{ asset('styles/img/icon/edit-2.svg') }}" class="pe-3" alt="">
-                                </a> -->
-                                <a href="{{ url('category', $category->id) }}/edit " class="text-decoration-none">
-                                    <img src="{{ asset('styles/img/icon/edit-2.svg') }}" class="pe-3" alt="">
-                                </a>
-                                <button type="submit" class="btn btn-transparent p-0 mb-1">
-                                    <img src="{{ asset('styles/img/icon/trash-2.svg') }}" alt="">
-                                </button>
-                            </form>
-                            <!-- <a href="" class="text-decoration-none">
-                                <img src="{{ asset('styles/img/icon/trash-2.svg') }}" alt="">
-                            </a> -->
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
+        <div class="row ps-5 pt-5">
+            <div class="col-5">
+                <button class="btn btn-transparent btn-lg shadow fw-bold fs-6" style="width: auto; height: 48px;">Add New Category</button>
+            </div>
+        </div>
+        <div class="row ps-5 pe-5">
+            <div class="col mt-4">
+                <form action="/category/{{$editcategory->id}}" method="POST">
+                    @csrf
+                    @method("PUT")
+                    <div class="mb-4 fs-6">
+                        <input type="text" value="{{$editcategory->name}}" name="name" class="form-control ps-3 shadow border-0" style="font-size: 14px; width: 100%; height: 48px;" id="exampleFormControlInput1" placeholder="Type name here ..">
+                    </div>
+                    <div class="mb-4">
+                        <textarea name="description" class="form-control ps-3 pt-3 shadow border-0" style="font-size: 14px;" placeholder="Type description here .." id="exampleFormControlTextarea1" rows="3">{{$editcategory->description}}</textarea>
+                    </div>
+                    <div class="d-flex justify-content-end" style="height: 48px;">
+                        <button type="submit" class="btn btn-secondary rounded fw-bold shadow" style="width: 130px;">Update</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
-    <div class="col-5 mt-4">
-        <form action="/category/{{$editcategory->id}}" method="POST">
-            @csrf
-            @method("PUT")
-            <div class="mb-4 fs-6">
-                <input type="text" value="{{$editcategory->name}}" name="name" class="fw-normal form-control ps-3 text-primary shadow border-0" style="font-size: 14px; width: 100%; height: 48px;" id="exampleFormControlInput1" placeholder="Type name here ..">
-            </div>
-            <div class="mb-4">
-                <textarea name="description" class="form-control ps-3 pt-3 text-primary shadow fw-normal border-0" style="font-size: 14px;" id="description" rows="3">{{$editcategory->description}}
-                </textarea>
-            </div>
-            <div class="d-flex justify-content-end" style="height: 48px;">
-                <button type="submit" class="btn btn-primary rounded fw-bold shadow" style="width: 130px;">Update</button>
-            </div>
-        </form>
+    <div class="col-5">
     </div>
 </div>
+
 @endsection
--- resources/views/template/base.blade.php
+++ resources/views/template/base.blade.php
@@ -2,137 +2,58 @@
 <html lang="en">
 
 <head>
+    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.80.0">
-    <title>@yield('title', 'Reorder')</title>
 
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/dashboard/">
-
-
-
-    <!-- Bootstrap core CSS -->
-    <link href="{{ asset('styles/css/bootstrap.min.css') }}" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-
-    <!-- Favicons -->
-    <link rel="apple-touch-icon" href="/docs/5.0/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
-    <link rel="icon" href="/docs/5.0/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
-    <link rel="icon" href="/docs/5.0/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
-    <link rel="manifest" href="/docs/5.0/assets/img/favicons/manifest.json">
-    <link rel="mask-icon" href="/docs/5.0/assets/img/favicons/safari-pinned-tab.svg" color="#7952b3">
-    <link rel="icon" href="/docs/5.0/assets/img/favicons/favicon.ico">
-    <meta name="theme-color" content="#7952b3">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link href="{{ asset('styles/css/reorder.css') }}" rel="stylesheet">
 
     <!-- Google Fonts -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
 
-    <!-- Feather icons -->
-    <script src="{{ asset('styles/js/feather.js') }}"></script>
-
-    <style>
-        .bd-placeholder-img {
-            font-size: 1.125rem;
-            text-anchor: middle;
-            -webkit-user-select: none;
-            -moz-user-select: none;
-            user-select: none;
-        }
-
-        @media (min-width: 768px) {
-            .bd-placeholder-img-lg {
-                font-size: 3.5rem;
-            }
-        }
-    </style>
-
-
-    <!-- Custom styles for this template -->
-    <link href="{{ asset('styles/css/dashboard.css') }}" rel="stylesheet">
+    <title>@yield('title', 'Reorder')</title>
 </head>
 
 <body>
-
-    <header class="navbar navbar-light sticky-top bg-transparent flex-md-nowrap p-0">
-        <a class="navbar-brand col-md-3 col-lg-2 me-0 ps-4 pt-4 bg-transparent" href="#">
-            <img src="{{ asset('styles/img/ReOrder.svg') }}" alt="">
-        </a>
-        <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <ul class="navbar-nav px-3">
-        </ul>
-    </header>
-
-    <div class="container-fluid">
-        <div class="row">
-            <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-tranparent sidebar collapse shadow">
-                <div class="position-sticky pt-3">
-                    <ul class="nav flex-column ps-2">
-                        <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="#">
-                                <span data-feather="home"></span>
-                                Dashboard
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
-                                <span data-feather="box"></span>
-                                Inventory
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
-                                <span data-feather="shopping-cart"></span>
-                                ReOrder Point
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
-                                <span data-feather="book"></span>
-                                Journal
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link {{request()->is('category')?'active': 'text-dark'}}" href="/category">
-                                <span data-feather="folder"></span>
-                                Category
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
-                                <span data-feather="file"></span>
-                                Product
-                            </a>
-                        </li>
-                        <li class="nav-item pt-5">
-                            <a class="nav-link" href="#">
-                                <span data-feather="power"></span>
-                                Sign Out
-                            </a>
-                        </li>
-                    </ul>
-
-                </div>
-            </nav>
-
-            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-                @yield('content')
-            </main>
+    <div class="row sticky-top shadow bg-white">
+        <div class="col-2 d-flex justify-content-center align-items-center shadow" style="height: 10vh;">
+            <a class="" href="#">
+                <img src="{{ asset('styles/img/ReOrder.svg') }}" class="" alt="" width="auto" height="24">
+            </a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-2 shadow p-5" style="height: 95vh">
+            <ul class="sidebar-link">
+                <a href="" class="text-decoration-none text-dark">
+                    <li>Dashboard</li>
+                </a>
+                <a href="/category" class="text-decoration-none text-dark">
+                    <li>Category</li>
+                </a>
+                <a href="" class="text-decoration-none text-dark">
+                    <li>Product</li>
+                </a>
+            </ul>
+        </div>
+        <div class="col-10">
+            @yield('content')
         </div>
     </div>
 
+    <!-- Optional JavaScript; choose one of the two! -->
 
-    <script src="/docs/5.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 
-    <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script>
-    <script src="dashboard.js"></script>
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+    -->
 </body>
-<script>
-    feather.replace()
-</script>
 
 </html>
